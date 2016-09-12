273ef6449f01fa063e37fa0c4d9394476a11ee35
Julian Gutierrez <juliangutierrezferrara@gmail.com>
2016-09-11 22:42:42 -0400

    jar with dependencies & fixing scripts

--- server.sh
+++ server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-export ROOT="." #Expected to be run at the git clone's location,otherwise adjust.
-export CLASSPATH="$ROOT/orderbook-server/target/classes:$ROOT/orderbook-api/target/classes"
-java com.example.orderbook.server.OrderBookServer 
+#export ROOT="." #Expected to be run at the git clone's location,otherwise adjust.
+#export CLASSPATH="$ROOT/orderbook-server/target/classes:$ROOT/orderbook-api/target/classes:~/.m2/repository/com/rabbitmq/amqp-client/3.5.1/*"
+#echo $CLASSPATH
+#java com.example.orderbook.server.OrderBookServer 
+java -jar orderbook-server/target/orderbook-server-1.0-SNAPSHOT.jar
